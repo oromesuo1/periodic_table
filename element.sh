fdfcbd57d139d29b6ba3b6202f437fb9f1cd1004
Michael B Obi <oromesuo1@gmail.com>
2022-04-19 10:44:48 +0100

    Create element.sh
    Imported element.sh file and add an explanatory comment.

---
+++ element.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+echo -e "Please provide an element as an argument."
+# Run button spins continuously immediately the code below this comment is uncommented. 
+#The button can only reset after the code is commented out again and the page is reloaded.
+
+# read USER_INPUT
+# MAIN (){
+# echo "$USER_INPUT"
+# }
+# MAIN
